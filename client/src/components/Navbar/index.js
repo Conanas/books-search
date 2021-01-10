7c3af7f6f170ca3b8bc1ee4e535de28fbdfadc3e
Tom Haigh <tmhaigh@gmail.com>
2021-01-11 09:30:06 +1100

    tidied up navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,16 +4,14 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="/">Books Search</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-            <a className="nav-link" href="#">Features</a>
-            <a className="nav-link" href="#">Pricing</a>
-            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <a className="nav-link active" aria-current="page" href="/">Search</a>
+            <a className="nav-link" href="/saved">Saved</a>
           </div>
         </div>
       </div>
