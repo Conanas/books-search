d0c0b7ead075adc7915217d5da90a08eb467383a
Tom Haigh <tmhaigh@gmail.com>
2021-01-11 09:45:20 +1100

    initial saerch form creation

---
+++ client/src/components/Search-Form/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function SearchForm() {
+  return (
+    <>
+      <header>
+        <h2>Book Search</h2>
+      </header>
+      <form>
+        <label>Book</label>
+        <input />
+        <input type="submit" />
+      </form>
+    </>
+  )
+}
