09426960e40190f4e34457fae6cb683b39ad7197
Tom Haigh <tmhaigh@gmail.com>
2021-01-11 09:45:04 +1100

    removed container fluid from navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+      <div className="container">
         <a className="navbar-brand" href="/">Books Search</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
