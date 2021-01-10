edb7615fa3b9dca1a8095b3a01c745d4df9938b7
Tom Haigh <tmhaigh@gmail.com>
2021-01-11 10:06:10 +1100

    added container class to form

--- client/src/components/Search-Form/index.js
+++ client/src/components/Search-Form/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './style.css';
 
 export default function SearchForm() {
   return (
-    <>
-      <header>
+    <form className="search-form container">
+      <header className="form-header">
         <h2>Book Search</h2>
       </header>
-      <form>
-        <label>Book</label>
-        <input />
-        <input type="submit" />
-      </form>
-    </>
+      <label>Book</label>
+      <input className="search-input" placeholder="Search for book title" />
+      <div className="submit-div">
+        <input className="form-search-submit" type="submit" />
+      </div>
+    </form>
   )
 }
