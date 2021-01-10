df67ef89b16830df8864daf607c8a3450ed63032
Tom Haigh <tmhaigh@gmail.com>
2021-01-11 10:36:59 +1100

    added intial results list item layout

--- client/src/components/Results-List/index.js
+++ client/src/components/Results-List/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import './style.css';
 
 export default function ResultsList() {
   return (
-    <>
-
-    </>
+    <div className="container">
+      <header>
+        <h2>Results</h2>
+      </header>
+      <ul className="results-list">
+        <li>
+          <header>
+            <div>
+              <h3>Harry Potter</h3>
+              <label>Written by: JK Rowling</label>
+            </div>
+            <div className="view-save-buttons">
+              <button>View</button>
+              <button>Save</button>
+            </div>
+          </header>
+          <div>
+            <img className="book-image" src="https://via.placeholder.com/150" alt="book" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div >
   )
 }
