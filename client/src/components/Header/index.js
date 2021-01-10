6f94faf3fbbd392d601120e0fdc8f960f9ed2c82
Tom Haigh <tmhaigh@gmail.com>
2021-01-11 10:05:27 +1100

    moved container class to header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,11 +3,9 @@ import './style.css';
 
 export default function Header() {
   return (
-    <header>
-      <div className="container">
-        <h1>React Books Search</h1>
-        <label>Search for and save books of interest</label>
-      </div>
+    <header className="page-header container">
+      <h1>React Books Search</h1>
+      <label>Search for and save books of interest</label>
     </header>
   )
 }
