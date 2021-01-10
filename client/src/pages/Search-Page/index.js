7313d625e3c7b06acbd6ba47c40eb0e1b19a4fb7
Tom Haigh <tmhaigh@gmail.com>
2021-01-11 10:06:51 +1100

    importing results list

--- client/src/pages/Search-Page/index.js
+++ client/src/pages/Search-Page/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchForm from '../../components/Search-Form/';
+import ResultsList from '../../components/Results-List/';
 
 export default function SearchPage() {
   return (
     <>
       <SearchForm />
+      <ResultsList />
     </>
   )
 }
