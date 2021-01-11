26b7cdfebd63dbbd6a03b03c2e83036b902103c7
Tom Haigh <tmhaigh@gmail.com>
2021-01-11 14:50:22 +1100

    imported Footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/Navbar/';
 import Header from './components/Header/';
+import Footer from './components/Footer/';
 import SearchPage from './pages/Search-Page/';
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="wrapper">
       <Navbar />
       <Header />
       <main>
@@ -17,7 +18,8 @@ function App() {
           </Switch>
         </Router>
       </main>
-    </>
+      <Footer />
+    </div>
   );
 }
 
