d91c7576cf7cd89ae08afed40f02da3e4c6cddd8
Tom Haigh <tmhaigh@gmail.com>
2021-01-11 14:49:42 +1100

    added mogoose connection

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+
+cons db = require('./models');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +13,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksearchdb", { useNewUrlParser: true });
 
 // Define API routes here
 
